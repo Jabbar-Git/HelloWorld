02182592076735211f318e1afd3c69ebf90f4671
ajabbar <ajabbar@Z2P2XISPX0045.corp.premierinc.com>
2024-03-23 12:37:26 -0400

    This is a first commit

---
+++ src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author ajabbar
+ *
+ */
+module yyy {
+}
---
+++ src/yyy/Test2.java
@@ -0,0 +1,15 @@
+package yyy;
+
+public class Test2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Helow World");
+		System.out.println("Helow World2");
+		System.out.println("Helow World3");
+		System.out.println("Helow World3");
+		System.out.println("Helow World4");
+
+	}
+
+}
